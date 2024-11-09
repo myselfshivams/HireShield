baa8653482e109cabd6a89e0ed22519dc2f2f88b
Sunscar-Sony <2201641530154@psit.ac.in>
2024-11-09 20:48:11 +0530

    icon added for tabbar

--- UrbanSphere-App/app/Tabs/_layout.tsx
+++ UrbanSphere-App/app/Tabs/_layout.tsx
@@ -1,26 +1,41 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';  // Import icons
 
 export default function TabLayout() {
-
   return (
     <Tabs
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <Tabs.Screen 
-      name="index" 
-      options={{ title: "Home" }} 
-    />
-    <Tabs.Screen 
-      name="Location" 
-      options={{ title: "Location" }} 
-    />
-    <Tabs.Screen 
-      name="Profile" 
-      options={{ title: "Profile" }} 
-    />
-  </Tabs>
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: "Home", 
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ) 
+        }} 
+      />
+      <Tabs.Screen 
+        name="Location" 
+        options={{ 
+          title: "Location", 
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="location-outline" size={size} color={color} />
+          ) 
+        }} 
+      />
+      <Tabs.Screen 
+        name="Profile" 
+        options={{ 
+          title: "Profile", 
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ) 
+        }} 
+      />
+    </Tabs>
   );
 }
